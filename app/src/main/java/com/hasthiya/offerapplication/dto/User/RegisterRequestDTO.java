08815e2cac77a82939d9d282ebaa273328974c51
nipunsampathperera <184128F@uom.lk>
2023-02-25 23:40:45 +0530

    add User Register API call

--- app/src/main/java/com/hasthiya/offerapplication/dto/User/RegisterRequestDTO.java
+++ app/src/main/java/com/hasthiya/offerapplication/dto/User/RegisterRequestDTO.java
@@ -1,8 +1,5 @@
 package com.hasthiya.offerapplication.dto.User;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 public class RegisterRequestDTO {
 
   private String name;
