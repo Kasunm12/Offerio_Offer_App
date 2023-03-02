4910db241e1e654d2e38b5b6905ec6cddb3fdf1e
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2023-03-03 00:49:07 +0530

    update to libarary and map activity

--- app/src/main/java/com/hasthiya/offerapplication/MapActivity.java
+++ app/src/main/java/com/hasthiya/offerapplication/MapActivity.java
@@ -3,15 +3,62 @@ package com.hasthiya.offerapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        //google map fragment
+//        SupportMapFragement mapFragement = (SupportMapFragement) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.signup_map);
+//        mapFragement.getMapAsync(OnMapReadyCallback this);
+        //google map Fragment
+
+
+    }
+
+    public void onMapReady(GoogleMap googleMap){
+
+        double startLatitude =6.767254 ;
+        double startLongitude = 79.8189226;
+        double endLatitude=6.9218387;
+        double endLongitude=79.8562055;
+
+        mMap= googleMap;
+        LatLng moratuwa= new LatLng(startLatitude,startLongitude);
+        mMap.addMarker(new MarkerOptions().position(moratuwa).title("Marker in Moratuwa"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moratuwa));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(startLatitude,startLongitude),15.0f));
+
+        //calculate distance
+        float[] results = new float[1];
+        Location.distanceBetween(startLatitude,startLongitude,endLatitude,endLongitude,results);
+        float distance = results[0];
+
+        int kilometre = (int) (distance/1000);
+
+        Toast.makeText(this, String.valueOf(kilometre)+" Km.", Toast.LENGTH_SHORT).show();
+
     }
 
 
